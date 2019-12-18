2b289e79aa68b88087b05b80f2efffaaf06287cd
AlexTD89 <trudov.alexandru@gmail.com>
2019-12-17 19:58:12 -0500

    12/17/19 - 2nd commit

---
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -0,0 +1,25 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+    @Before // run before scenario
+    public void setUP(){
+        System.out.println("\n\tthis is coming before scenario");
+    }
+
+    @After // run after scenario
+    public void tearDown(){
+        System.out.println("\tThis is coming from after scenario\n");
+    }
+
+    @After("@sales_manager") // will run with only those scenarios that have this given tag
+    public void tearDownSalesManager(){
+        System.out.println("This is coming from after scenario for sale managers\n");
+    }
+
+
+
+}
